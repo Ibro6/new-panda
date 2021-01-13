2e5c53f02ed7373d13896e33e04626deb4d6f8eb
Ibrokhim <ibrohimechanic@gmail.com>
2021-01-13 18:48:42 +0500

    feat:minor improvements

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import { onMouseClick, fixHeader, hideMouseIcon } from "./js/scroll.js";
+import {
+    onMouseClick,
+    fixHeader,
+    hideMouseIcon
+} from "./js/scroll.js";
 import "./css/style.scss";
+import anime from 'animejs/lib/anime.es.js';
 
 onMouseClick();
 fixHeader();
 hideMouseIcon();
+
+anime({
+    targets: '.features-item-title',
+    value: [0, 1000],
+    round: 1,
+    easing: 'easeInOutExpo'
+
+});
 
 console.log("it works")
---
+++ src/js/animejs.js
@@ -0,0 +1,9 @@
+import anime from 'animejs/lib/anime.es.js';
+
+ anime({
+    targets: 'features-item-title',
+    value: ['sjjn sdjds', 'QUALITY PRODUCTS'],
+    round: 1,
+    easing: 'easeInOutExpo'
+  });
+  
